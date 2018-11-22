3458de46644801da4a833903a3a2fbbcee8f70f6
Sharma, Amit <Asharma@agtinternational.com>
2018-11-22 12:59:17 +0100

    WIP: Initial commit project start up.... new page is displayed.

--- packages/german-docs/siteConfig.js
+++ packages/german-docs/siteConfig.js
@@ -5,92 +5,94 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// See https://docusaurus.io/docs/site-config for all the possible
+// See https://docusaurus.io/docs/site-config.html for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
+/* List of projects/orgs using your project for the users page */
 const users = [{
-  caption: 'User1',
-  // You will need to prepend the image path with your baseUrl
-  // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-  image: '/img/docusaurus.svg',
-  infoLink: 'https://www.facebook.com',
+  caption: 'Amit Sharma',
+  image: '',
+  infoLink: '',
   pinned: true,
-}, ];
+}];
 
 const siteConfig = {
-  customDocsPath: 'documentation/docs',
-  title: 'Documenattion', // Title for your website.
-  tagline: 'A website for testing',
-  url: '', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  customDocsPath: 'german-docs/docs',
+  title: 'Technical Documentation' /* title for your website */ ,
+  tagline: 'Website for all technical documentation...',
+  url: '',
+  cleanUrl: false,
+
+  baseUrl: '/' /* base url for your project */ ,
 
   // Used for publishing and more
-  projectName: 'Documentation-101',
-  organizationName: 'Sharmas',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+  projectName: 'project-documents',
+  organizationName: 'Sharma',
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [],
+  headerLinks: [{
+      doc: 'german-tutorial/prepositions/prepositions',
+      label: 'prepositions'
+    },
+    {
+      page: 'help',
+      label: 'Help'
+    },
+    // { blog: true, label: 'Blog' },
+  ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/docusaurus.svg',
-  footerIcon: 'img/docusaurus.svg',
-  favicon: 'img/favicon.png',
+  headerIcon: 'img/project-logo.png',
+  footerIcon: 'img/project-logo.png',
+  favicon: 'img/project-logo.png',
 
-  /* Colors for website */
+  blogSidebarCount: 'ALL',
+
+  /* colors for website */
   colors: {
-    primaryColor: '#2E8555',
-    secondaryColor: '#205C3B',
+    primaryColor: '#20232a',
+    secondaryColor: '#e91e39',
+    // primaryColor: '#2E8555',
+    // secondaryColor: '#205C3B',
   },
 
-  /* Custom fonts for website */
-  /*
+  /* custom fonts for website */
   fonts: {
     myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
       "-apple-system",
-      "system-ui"
+      "BlinkMacSystemFont",
+      "Segoe UI",
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+      "sans-serif",
     ]
   },
-  */
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  copyright: 'Copyright © ' +
+    new Date().getFullYear() +
+    ' Sharma',
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
+    // Highlight.js theme to use for syntax highlighting in code blocks
     theme: 'default',
   },
 
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  // Add custom scripts here that would be placed in <script> tags
+  scripts: [
+    // 'https://buttons.github.io/buttons.js'
+  ],
 
-  // On page navigation for the current documentation page.
+  /* On page navigation for the current documentation page */
   onPageNav: 'separate',
-  // No .html extensions for paths.
-  cleanUrl: true,
-
-  // Open Graph and Twitter card images.
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
-
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
-
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
